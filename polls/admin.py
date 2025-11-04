c556b9cf3f4b8109ed499d6effbce9b83a8b5262
Maxime Pigeon <me@maximepigeon.com>
2025-11-04 10:37:58 -0500

    Class 2: Create models for polls

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Poll)
+admin.site.register(models.Question)
+admin.site.register(models.Choice)
+admin.site.register(models.Entry)
+admin.site.register(models.Answer)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField()
+    description = models.TextField(blank=True)
+    pub_date = models.DateField("publication date")
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    text = models.CharField()
+    order = models.PositiveIntegerField()
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["order", "text"]
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    text = models.CharField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
+class Entry(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"Entry for {self.poll} on {self.date}"
+
+
+class Answer(models.Model):
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Answer for {self.entry}"
