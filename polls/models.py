156536efaf630e58e93c2be026477a631a4db819
Maxime Pigeon <me@maximepigeon.com>
2025-11-04 10:37:58 -0500

    Class 2: Create models for polls

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField()
+    description = models.TextField(blank=True)
+    pub_date = models.DateTimeField("publication date")
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    text = models.CharField()
+    order = models.PositiveIntegerField()
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["order", "text"]
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    text = models.CharField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    votes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.text
