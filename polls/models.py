745cc116aa243771be2a7ec857527dea07e4e230
Maxime Pigeon <me@maximepigeon.com>
2025-11-21 11:14:14 -0500

    Class 3: Create polls views

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,21 @@
+from typing import Self
+
 from django.db import models
+from django.utils import timezone
 
 
 class Poll(models.Model):
     title = models.CharField()
     description = models.TextField(blank=True)
     pub_date = models.DateTimeField("publication date")
+
+    @classmethod
+    def published_polls(cls) -> list[Self]:
+        return [poll for poll in cls.objects.all() if poll.is_published]
+
+    @property
+    def is_published(self) -> bool:
+        return self.pub_date < timezone.now()
 
     def __str__(self):
         return self.title
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,37 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+
+from .models import Poll
 
 
 def index(request):
-    return HttpResponse("Hello, World")
+    body = "\n".join(
+        f"{poll.id} {poll.title}" for poll in Poll.published_polls()
+    )
+    return HttpResponse(body, content_type="text/plain")
+
+
+def detail(request, poll_id: int):
+    poll = Poll.objects.get(pk=poll_id)
+    questions = poll.question_set.all()
+    body = f"{poll.title}\n\n"
+    for question in questions:
+        body += "\t" + question.text + "\n\n"
+        choices = question.choice_set.all()
+        body += "\n".join(f"\t\t{choice.text}" for choice in choices) + "\n\n"
+    return HttpResponse(body, content_type="text/plain")
+
+
+def results(request, poll_id: int):
+    poll = Poll.objects.get(pk=poll_id)
+    questions = poll.question_set.all()
+    body = f"{poll.title}\n\n"
+    for question in questions:
+        body += "\t" + question.text + "\n\n"
+        choices = question.choice_set.order_by("-votes")
+        body += (
+            "\n".join(
+                f"\t\t{choice.votes}\t{choice.text}" for choice in choices
+            )
+            + "\n\n"
+        )
+    return HttpResponse(body, content_type="text/plain")
